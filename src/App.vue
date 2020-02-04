05226958076acef50427937f53e874b739052415
Kshitij Rajvansh <kshrajva@publicisgroupe.net>
2020-02-04 10:25:47 +0530

    Initial commit for meetings app

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,127 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <Navigation v-bind:user="user" @logout="logout" />
+    <router-view
+      class="container"
+      v-bind:user="user"
+      v-bind:meetings="meetings"
+      v-bind:error="error"
+      @logout="logout"
+      @addMeeting="addMeeting"
+      @deleteMeeting="deleteMeeting"
+      @checkin="checkin"
+    />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Navigation from "@/components/Navigation.vue";
 
-#nav {
-  padding: 30px;
+import firebase from "firebase";
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+import db from "../src/db";
 
-    &.router-link-exact-active {
-      color: #42b983;
+const firestore = db.firestore();
+
+export default {
+  name: "app",
+  data: function() {
+    return {
+      user: null,
+      error: null,
+      meetings: []
+    };
+  },
+  methods: {
+    logout: function() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.user = null;
+          this.$router.push("login");
+        });
+    },
+    addMeeting: function(payload) {
+      firestore
+        .collection("users")
+        .doc(this.user.uid)
+        .collection("meetings")
+        .add({
+          name: payload,
+          createdAt: firebase.firestore.FieldValue.serverTimestamp()
+        });
+    },
+    deleteMeeting: function(payload) {
+      firestore
+        .collection("users")
+        .doc(this.user.uid)
+        .collection("meetings")
+        .doc(payload)
+        .delete();
+    },
+    checkin: function(payload) {
+      firestore.collection("users")
+        .doc(payload.userID)
+        .collection("meetings")
+        .doc(payload.meetingID)
+        .get()
+        .then(doc => {
+          if(doc.exists) {
+            firestore.collection("users")
+              .doc(payload.userID)
+              .collection("meetings")
+              .doc(payload.meetingID)
+              .collection("attendies")
+              .add({
+                displayName: payload.displayName,
+                email: payload.email,
+                createdAt: firebase.firestore.FieldValue.serverTimestamp()
+              })
+              .then(() => {
+                this.$router.push("/");
+              });
+          } else {
+            this.error = "Sorry, No such meeting exists!";
+          }
+        });
     }
+  },
+  mounted() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.user = user;
+
+        firestore
+          .collection("users")
+          .doc(this.user.uid)
+          .collection("meetings")
+          .onSnapshot(snapshot => {
+            const snapData = [];
+            snapshot.forEach(doc => {
+              snapData.push({
+                id: doc.id,
+                name: doc.data().name
+              });
+            });
+            this.meetings = snapData.sort((a, b) => {
+              if (a.name.toLowerCase() < b.name.toLowerCase()) {
+                return -1;
+              } else {
+                return 1;
+              }
+            });
+          });
+      }
+    });
+  },
+  components: {
+    Navigation
   }
-}
+};
+</script>
+
+<style lang="scss">
+$primary: #05b2dd;
+@import "../node_modules/bootstrap/scss/bootstrap";
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <nav class="site-nav navbar navbar-expand bg-primary navbar-dark">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/">Meetings Logo</router-link>
+        <div class="navbar-nav ml-auto">
+          <router-link class="nav-item nav-link" to="/meetings" v-if="user">
+            Meetings
+          </router-link>
+          <router-link class="nav-item nav-link" to="/register" v-if="!user">
+            Register
+          </router-link>
+          <router-link class="nav-item nav-link" to="/login" v-if="!user">
+            Log In
+          </router-link>
+          <button class="nav-item nav-link btn btn-link" to="/login" role="button" v-if="user" @click="$emit('logout')">
+            Logout
+          </button>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "navigation",
+  props: ["user"]
+};
+</script>
